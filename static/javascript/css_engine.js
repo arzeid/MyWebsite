81ffad7dc844db418a60bf43d861c713fbcce413
arzeid <arzeid.1994@gmail.com>
2017-01-20 00:01:47 -0500

    changed mobile-navigaton bar and fixed resizing issues

--- static/javascript/css_engine.js
+++ static/javascript/css_engine.js
@@ -7,10 +7,22 @@ $(function(){
     if(b.prop('scrollHeight')>b.height()){
         normalWidth = window.innerWidth;
         scrollWidth = normalWidth - b.width();
-        $('#container').css({marginRight:'-'+scrollWidth+'px'});
-        $('#container').css({paddingRight:'-'+scrollWidth+'px'});
+        $('body').css({marginRight:'-'+scrollWidth+'px'});
     }
 });
+*/
+
+/*
+function scrollbarWidth() { 
+    var div = $('<div style="width:50px;height:50px;overflow:hidden;position:absolute;top:-200px;left:-200px;"><div style="height:100px;"></div>'); 
+    // Append our div, do our calculation and then remove it 
+    $('body').append(div); 
+    var w1 = $('div', div).innerWidth(); 
+    div.css('overflow-y', 'scroll'); 
+    var w2 = $('div', div).innerWidth(); 
+    $(div).remove(); 
+    return (w1 - w2); 
+}
 */
 
 $(document).ready(function(){
@@ -19,12 +31,16 @@ $(document).ready(function(){
         $(this).siblings(".dropdown").find(".dropdown-content").hide();
         $(this).find(".dropdown-content").toggle();
     });
-    $("#topnav-menu").click(function(e){
-        $(this).siblings(".topnav").slideToggle();
+    $("#topnav-menu-button").click(function(e){
+        $(this).parent().siblings(".topnav").slideToggle();
     });
+
     $(window).resize(function() {
-        if($(window).width() > 500) {
+        if($("#topnav-menu-button").is( ":hidden" )){
             $(".topnav").show();
+        }
+        else if($("#topnav-menu-button").is( ":visible" )){
+            $(".topnav").hide();
         }
     });
 });
