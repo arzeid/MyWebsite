d05034e6ca9c9ca6a3e7b1d797c243c295b82c3d
arzeid <arzeid.1994@gmail.com>
2017-01-22 16:46:03 -0500

    shorten css redesigned and url validation moved to front end

--- static/javascript/shorten.js
+++ static/javascript/shorten.js
@@ -11,3 +11,66 @@ setInterval(function(){
     get_url_count()
 }, 10000);
 */
+
+function ValidURL(str) {
+  var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
+    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
+    '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
+    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
+    '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
+    '(\#[-a-z\d_]*)?$','i'); // fragment locater
+  if(!pattern.test(str)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+
+function isValidUrl(url){
+    var myVariable = url;
+    var pattern = /^(http|https|ftp):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i;
+    if(pattern.test(myVariable)) {
+        return true;
+    } else {
+        return false;
+    }   
+}
+
+
+$(document).ready(function(){
+
+    $(".urlinput-button").click(function(){
+        var url = $('.urlinput').val();
+        
+        if (isValidUrl(url)==true){
+            //$('.urlinput-button').val("Shorten "+String.fromCharCode(10004));
+            $('.urlinput-button').prop('disabled', false);
+            $('.urlinput-button').val("Shorten");
+        }
+        else{
+            $('.urlinput-button').val("Invalid URL");
+            $('.urlinput-button').prop('disabled', true);
+            //$('.urlinput').addClass('red');
+        }
+    });
+    
+    $('input').keyup(function() {
+        var url = $(this).val();
+        
+        if (isValidUrl(url)==true){
+            //$('.urlinput-button').val("Shorten "+String.fromCharCode(10004));
+            $('.urlinput-button').prop('disabled', false);
+            $('.urlinput-button').val("Shorten");
+        }
+        else{
+            $('.urlinput-button').val("Invalid URL");
+            $('.urlinput-button').prop('disabled', true);
+            //$('.urlinput').addClass('red');
+        }
+     
+    });
+});
+
+
+
