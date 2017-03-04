18ed17820f23f1f06d5ab19ad8eddaa5516a0380
arzeid <arzeid.1994@gmail.com>
2017-03-03 19:56:29 -0500

    redesigned blog home

--- static/javascript/blog.js
+++ static/javascript/blog.js
@@ -4,13 +4,18 @@ $(document).ready(function(){
 
     $(".arrow").click(function(e){
 		var postBox = $(this).siblings(".post-box");
+        var postTitle = $(this).siblings(".post-title");
         if(postBox.is( ":hidden" )){
+            postTitle.css("opacity","1");
+            $(this).css("opacity","1");
             postBox.slideToggle();
             $(this).text(String.fromCharCode(9652));
         }
         else if(postBox.is( ":visible" )){
             postBox.slideToggle();
             $(this).text(String.fromCharCode(9662));
+            postTitle.css("opacity","0.4");
+            $(this).css("opacity","0.4");
         }
     });
 });
