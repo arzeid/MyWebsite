ffd66bf19e41b04c3563f8f33b2dd74e65d2277c
arzeid <arzeid.1994@gmail.com>
2017-03-05 15:43:51 -0500

    completed blog home

--- static/javascript/blog.js
+++ static/javascript/blog.js
@@ -3,19 +3,17 @@
 $(document).ready(function(){
 
     $(".arrow").click(function(e){
-		var postBox = $(this).siblings(".post-box");
-        var postTitle = $(this).siblings(".post-title");
+		var postBox = $(this).parent().siblings(".post-box");
+        var postTitle = $(this).parent();
         if(postBox.is( ":hidden" )){
-            postTitle.css("opacity","1");
-            $(this).css("opacity","1");
+            postTitle.children().css("opacity","1");
             postBox.slideToggle();
             $(this).text(String.fromCharCode(9783));
         }
         else if(postBox.is( ":visible" )){
             postBox.slideToggle();
             $(this).text(String.fromCharCode(9776));
-            postTitle.css("opacity","0.4");
-            $(this).css("opacity","0.4");
+            postTitle.children().css("opacity","0.4");
         }
     });
 });
